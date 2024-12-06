7234276caded8caf9e8cc5fc9afd861c7fa949e7
Gabriel <g281770@dac.unicamp.br>
2024-12-06 12:20:36 -0300

    fixed user auth

--- src/auth/auth_service.py
+++ src/auth/auth_service.py
@@ -6,7 +6,7 @@ from schema.mih.schema_mih import User  # Importe o modelo User do banco de dado
 class AuthService:
     @staticmethod
     def get_current_user(request: Request):
-        credentials = request.session.get("credentials")
-        if not credentials or not credentials.get("token"):
+        user_id = request.session.get("id")
+        if not user_id:
             raise HTTPException(status_code=401, detail="User not authenticated")
         return request.session.get("email")
